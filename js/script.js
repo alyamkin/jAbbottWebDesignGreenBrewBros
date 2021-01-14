54b376a3cecaeca9fce8787f44395c04272965b1
Andrey Lyamkin <andrew.lyamkin@gmail.com>
2021-01-13 20:49:29 -0500

    Add galleries

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Slider functionality
+$(document).ready(function () {
+  $(".event-gallery-2,.event-gallery-1").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
